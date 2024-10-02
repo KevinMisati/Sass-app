09a85f2296db70995b34b9c6368c616cc8a3004c
KevinMisti <kevinmosigisi2001@gmail.com>
2024-10-02 07:07:11 +0300

    added the docker file and railway.toml for deployment to railway

--- src/kevinhome/settings.py
+++ src/kevinhome/settings.py
@@ -25,7 +25,14 @@ SECRET_KEY = 'django-insecure-t*2_(=4gyx^w2oh(sv*-21i#ol5!$a2#8-8&l(39i=!0+$8dn1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app"
+]
+if DEBUG:
+    ALLOWED_HOSTS += [
+        '127.0.0.1',
+        'localhost'
+    ]
 
 
 # Application definition
