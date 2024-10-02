efca585ee4c2dd435533a451f84ba5ee6a1471c7
KevinMisti <kevinmosigisi2001@gmail.com>
2024-10-02 08:32:58 +0300

    added environment variables

--- src/kevinhome/settings.py
+++ src/kevinhome/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("Django_SECRET_KEY",default=None)
+SECRET_KEY = config("DJANGO_SECRET_KEY",default=None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower == "true" 
